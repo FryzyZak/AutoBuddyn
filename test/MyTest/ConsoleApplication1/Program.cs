f64e3760d1ee119d386881dfabb9a1400439a66b
AkumetSu <otaku.akumetsu@gmail.com>
2016-03-09 15:03:41 -0400

    Close the game Addon... for  rohanlovecs.

---
+++ test/MyTest/ConsoleApplication1/Program.cs
@@ -0,0 +1,17 @@
+﻿using EloBuddy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Game.QuitGame();
+        }
+    }
+}
