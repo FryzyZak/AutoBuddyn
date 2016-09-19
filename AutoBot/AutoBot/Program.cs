c31a2a6a8d22df74aadcbe17b96a882ade519630
Dev Akumetsu <otaku.akumetsu@gmail.com>
2016-09-19 16:27:27 -0400

    Changes to AutoBot still not even in Beta.

--- AutoBot/AutoBot/Program.cs
+++ AutoBot/AutoBot/Program.cs
@@ -16,9 +16,12 @@ namespace AutoBot
     {
         public static string Author = "devAkumetsu";
         public static string AddonName = "AutoBot";
+        public static Menu Config;
+        public static Random Rnd = new Random(Environment.TickCount);
 
         static void Main(string[] args)
         {
+
             Hacks.DisableTextures = true;
             Hacks.AntiAFK = true;
             Hacks.RenderWatermark = false;
@@ -32,14 +35,37 @@ namespace AutoBot
 
             Version v = Assembly.GetExecutingAssembly().GetName().Version;
             string scriptVersion = v.Major + "." + v.MajorRevision + "." + v.Minor + "." + v.MinorRevision;
+            // version of addon
+            Chat.Print(AddonName + " made by " + Author + " loaded!");            
+            Chat.Print("Version Loaded" + scriptVersion); 
 
-            Chat.Print(AddonName + " made by " + Author + " loaded!");  // set version          
-            Chat.Print("Version Loaded" + scriptVersion); // set version
+            // menu AutoBot
+            Config = MainMenu.AddMenu("Auto Bot", "AutoBot");
+            Config.Add("activeab", new CheckBox("Active the AutoBot?"));
+            Config.Add("textureab", new CheckBox("Show Texture? (f5 to load)"), false);
+
+            Game.OnUpdate += On_Update;
 
         }
 
+        private static void On_Update(EventArgs args)
+        {
+            if (!Config["activeab"].Cast<CheckBox>().CurrentValue)
+                return;
+                      
+            // Chat config
+            if (Player.Instance.IsDead)
+            {               
+                return;
+            }
+        }
+        
+
         private static void ManagedTexture_OnLoad(OnLoadTextureEventArgs args)
         {
+            if (!Config["textureab"].Cast<CheckBox>().CurrentValue)
+                return;
+
             args.Process = false;
         }
     }
