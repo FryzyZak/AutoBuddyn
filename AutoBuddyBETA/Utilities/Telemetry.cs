f28aff5fa0e7092745b8e701aaba267d3a7d6e50
Dev Akumetsu <otaku.akumetsu@gmail.com>
2016-11-14 15:59:10 -0400

    Fix telemetry Errors.

--- AutoBuddyBETA/Utilities/Telemetry.cs
+++ AutoBuddyBETA/Utilities/Telemetry.cs
@@ -36,66 +36,27 @@ namespace AutoBuddy.Utilities
         }
         public static void SendEvent(string type, Dictionary<string, string>data )
         {
-            BackgroundWorker bw3 = new BackgroundWorker();
-            bw3.DoWork += bw3_DoWork;
+            BackgroundWorker bw3 = new BackgroundWorker();          
             bw3.RunWorkerAsync(new object[] { type, data });
         }
-
-        private static void bw3_DoWork(object sender, DoWorkEventArgs e)
-        {
-            object[] args = (object[])e.Argument;
-            Dictionary<string, string> arg = (Dictionary<string, string>) args[1];
-            foreach (var kv in new Dictionary<string, string>()
-            {
-                {"id", id},
-                {"type", "Event"},
-                {"eventType", (string) args[0]},
-            })
-                arg.Add(kv.Key, kv.Value);
-            string x ="http://autobuddy.tk/ann/d2.php".Post(arg);
-            while(!x.Contains("thanks"))
-            {
-                if(x.Contains("error 1"))
-                    getId();
-                Thread.Sleep(5000);
-                arg["id"] = id;
-                x ="http://autobuddy.tk/ann/d2.php".Post(arg);
-                
-            }
-
-        }
+               
 
         public static void SendFileAndDelete(string file, string name)
         {
 
-            BackgroundWorker bw2 = new BackgroundWorker();
-            bw2.DoWork += bw2_DoWork;
+            BackgroundWorker bw2 = new BackgroundWorker();        
             bw2.RunWorkerAsync(new[]{file, name});
         }
 
-        private static void bw2_DoWork(object sender, DoWorkEventArgs e)
-        {
-            string[] args = (string[])e.Argument;
-            if (!File.Exists(args[0])) return;
-            string result = "http://autobuddy.tk/ann/d2.php".Post(new Dictionary<string, string>() { { "id", id }, { "GameID", AutoWalker.GameID }, { "type", "File" }, { "fileType", args[1] }, { "data", File.ReadAllText(args[0]) } });
-            if (result.Contains("thanks"))
-                File.Delete(args[0]);
-        }
-
+     
 
         private static void getId()
         {
-            BackgroundWorker bw = new BackgroundWorker();
-            bw.DoWork += bw_DoWork;
+            BackgroundWorker bw = new BackgroundWorker();         
             bw.RunWorkerCompleted += bw_RunWorkerCompleted;
-
             bw.RunWorkerAsync();
         }
-        private static void bw_DoWork(object sender, DoWorkEventArgs e)
-        {
-            e.Result="http://autobuddy.tk/ann/d2.php".Post(new Dictionary<string, string>() {{"type", "GetID"}});
-
-        }
+      
 
         private static void bw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
