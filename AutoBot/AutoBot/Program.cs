8aa2ea360b97f9abdcb559686cf4d2d7d985b7b5
Dev Akumetsu <otaku.akumetsu@gmail.com>
2016-11-14 14:24:53 -0400

    Update AutoBot, fix error install for restart the testing.

--- AutoBot/AutoBot/Program.cs
+++ AutoBot/AutoBot/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using EloBuddy;
 using EloBuddy.SDK;
 using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
 using EloBuddy.SDK.Events;
+using EloBuddy.Sandbox;
 using Version = System.Version;
 using System.Reflection;
 
@@ -35,9 +40,9 @@ namespace AutoBot
 
             // menu AutoBot
             Config = MainMenu.AddMenu("Auto Bot", "AutoBot");
-            Config.Add("activeab", new CheckBox("Active the AutoBot?"));
-            Config.Add("hacksab", new CheckBox("Active Texture? (f5 to load)"), false);
-            Config.Add("chattingab", new CheckBox("Active Chatting BOT?"), false);
+            Config.Add("menu_active_autobot", new CheckBox("Active the AutoBot?"), true);
+            Config.Add("menu_hacks_autobot", new CheckBox("Active Texture? (f5 to load)"), false);
+            Config.Add("menu_chatting_autobot", new CheckBox("Active Chatting BOT?"), false);
 
             Game.OnUpdate += On_Update;
 
@@ -46,17 +51,19 @@ namespace AutoBot
         private static void On_Update(EventArgs args)
         {
             // check if we need to active the addon ifself
-            if (!Config["activeab"].Cast<CheckBox>().CurrentValue)
-                return;
+                if (!Config["menu_active_autobot"].Cast<CheckBox>().CurrentValue)
+                    return;
 
             // Setup hacks texture for BOTTING
-            if (!Config["hacksab"].Cast<CheckBox>().CurrentValue)
-                Hacks.Init();
+            if (!Config["menu_hacks_autobot"].Cast<CheckBox>().CurrentValue) { 
+                   // Hacks.Init();
+            }
 
             // Setup Chatting BOT
-            if (!Config["chattingab"].Cast<CheckBox>().CurrentValue)
-                Chatting.Init();                      
-        }      
-        
+            if (!Config["menu_chatting_autobot"].Cast<CheckBox>().CurrentValue) { 
+                  //  Chatting.Init();
+            }
+        }
+
     }
 }
